416f23702375b1cde387118c60ba96079cdee8e9
yippptay <yippptay@gmail.com>
2025-03-20 17:32:40 -0700

    main.js

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,14 +1,14 @@
 import Head from 'next/head'
-import dynamic from 'next/dynamic'
+// import dynamic from 'next/dynamic'
 import NavBar from '../navbar'
 import { Box, Container } from '@chakra-ui/react'
 import Footer from '../footer'
-import VoxelDogLoader from '../voxel-dog-loader'
+// import VoxelDogLoader from '../voxel-dog-loader'
 
-const LazyVoxelDog = dynamic(() => import('../voxel-dog'), {
-  ssr: false,
-  loading: () => <VoxelDogLoader />
-})
+// const LazyVoxelDog = dynamic(() => import('../voxel-dog'), {
+//   ssr: false,
+//   loading: () => <VoxelDogLoader />
+// })
 
 const Main = ({ children, router }) => {
   return (
@@ -35,7 +35,7 @@ const Main = ({ children, router }) => {
       <NavBar path={router.asPath} />
 
       <Container maxW="container.md" pt={14}>
-        <LazyVoxelDog />
+        {/* <LazyVoxelDog /> */}
 
         {children}
 
