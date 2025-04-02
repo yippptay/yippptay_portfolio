ec065515a8d58d3d3b55468d5bbab49e1b8a19fd
yippptay <yippptay@gmail.com>
2025-04-02 14:40:33 -0700

    meta name

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -13,6 +13,7 @@ import Footer from '../footer'
 const Main = ({ children, router }) => {
   return (
     <Box as="main" pb={8}>
+      
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Taylor's homepage" />
@@ -20,10 +21,10 @@ const Main = ({ children, router }) => {
         <meta name="author" content="yippptay" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <meta name="twitter:title" content="Takuya Matsuyama" />
+        <meta name="twitter:title" content="Taylor Yip" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@craftzdog" />
-        <meta name="twitter:creator" content="@craftzdog" />
+        <meta name="twitter:site" content="@lmaoyip" />
+        <meta name="twitter:creator" content="@lmaoyip" />
         <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
         <meta property="og:site_name" content="Taylor Yip" />
         <meta name="og:title" content="Taylor Yip" />
