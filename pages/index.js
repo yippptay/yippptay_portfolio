7482e8d58ff8495edfec60e37efe27d535806bae
yippptay <yippptay@gmail.com>
2025-04-25 15:49:24 -0700

    IoLogoGithub hotfix

--- pages/index.js
+++ pages/index.js
@@ -16,9 +16,9 @@ import { BioSection, BioYear } from '../components/bio'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
 // import { GridItem } from '../components/grid-item'
-import { IoLogoGithub } from 'react-icons/io5'
+// import { IoLogoGithub } from 'react-icons/io5'
 // import thumbYouTube from '../public/images/links/youtube.png'
-// import thumbInkdrop from '../public/images/works/inkdrop_eyecatch.png'
+// import thumbInkdrop from '../public/images/works/inkdrop_eyecatch.png' 
 import Image from 'next/image'
 
 const Home = () => (
